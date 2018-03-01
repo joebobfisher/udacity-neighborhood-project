1d856978442025593420c50c997e59b0ac976eb8
Joe Fisher <fisher.joe@gmail.com>
2018-02-28 21:23:27 -0600

    Added 4 more coordinates (hard-coded for now).

--- js/my.js
+++ js/my.js
@@ -1,20 +1,42 @@
-// Map object
+// Map Object
 var Neighborhood;
 
-// House coordinates
-var MyHouseCoordinates = {lat: 42.480018, lng:-92.363529};
-
-
+// Place Coordinates
+var HouseCoordinates =      {lat: 42.480018, lng: -92.363529};
+var WorkCoordinates =       {lat: 42.478491, lng: -92.454093};
+var GroceriesCoordinates =  {lat: 42.458456, lng: -92.330557};
+var ChurchCoordinates =     {lat: 42.515738, lng: -92.413237};
+var LibraryCoordinates =    {lat: 42.496388, lng: -92.340966};
 
 // Map initialization
 // TODO: parameterize center
 function startMap() {
-  Neighborhood = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14, center: MyHouseCoordinates
+  var Neighborhood = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14, center: HouseCoordinates
   });
 
-  Marker_MyHouse = new google.maps.Marker({
-    position: MyHouseCoordinates,
+  var Marker_House = new google.maps.Marker({
+    position: HouseCoordinates,
+    map: Neighborhood
+  });
+
+  var Marker_Work = new google.maps.Marker({
+    position: WorkCoordinates,
+    map: Neighborhood
+  });
+
+  var Marker_Groceries = new google.maps.Marker({
+    position: GroceriesCoordinates,
+    map: Neighborhood
+  });
+
+  var Marker_Church = new google.maps.Marker({
+    position: ChurchCoordinates,
+    map: Neighborhood
+  });
+
+  var Marker_Library = new google.maps.Marker({
+    position: LibraryCoordinates,
     map: Neighborhood
   });
 }
