3fb2921524bd58838fd8f472e8afa274c860e7fb
Joe Fisher <fisher.joe@gmail.com>
2018-03-11 15:06:39 -0500

    Added foursquare API (not using it in infowindow yet, though).

--- js/my.js
+++ js/my.js
@@ -1,6 +1,5 @@
 // Initial data
 var initialPlaces = [
-  { name: "House",         loc: { lat: 42.480018, lng: -92.363529 }, dist: 0 },
   { name: "Work",          loc: { lat: 42.478491, lng: -92.454093 }, dist: 6.2 },
   { name: "Grocery Store", loc: { lat: 42.458456, lng: -92.330557 }, dist: 3.1 },
   { name: "Church",        loc: { lat: 42.515738, lng: -92.413237 }, dist: 4.6 },
@@ -34,7 +33,7 @@ var ViewModel = function() {
   this.places = ko.observableArray([]);
 
   for (var i=0; i<initialPlaces.length; i++) {
-    self.places.push(new Place(i, initialPlaces[i]));
+    self.places().push(new Place(i, initialPlaces[i]));
   }
 
   // currently selected place
@@ -45,9 +44,7 @@ var ViewModel = function() {
 
   this.setPlace = function(clickedPlace) {
     self.place(clickedPlace);
-
     Neighborhood.setCenter(self.place().loc());
-
     doInfoWindow(self.place().id(), infoWindow);
   };
 
@@ -103,7 +100,7 @@ function startMap() {
 
     marker.addListener('click', function() {
       // add a listener for doInfoWindow() to last marker we pushed
-      doInfoWindow(vm.markers.length-1, infoWindow);
+      doInfoWindow(this.id, infoWindow);
     });
 
     bounds.extend(marker.position);
@@ -114,13 +111,34 @@ function startMap() {
 
 function doInfoWindow(i, infoWindow) {
   var marker = vm.markers[i];
+  var fsqData = {};
 
   if (infoWindow.marker != marker) {
+    $.getJSON("https://api.foursquare.com/v2/venues/search",
+              { client_id: 'PPLTRCLFQWHDYILXAT3G3B0RLK15VFLQIUT4KT51AXAVFAL2',
+                client_secret: 'D2FQPOZDFUQGYURIAGEJZRYN1XG3BFRYNEHWXSR1DPF41HYY',
+                v: '20170801',
+                ll: vm.places()[i].loc().lat + "," + vm.places()[i].loc().lng,
+                limit: 1 })
+      .done(function(json) {
+        fsqData = json;
+      })
+      .fail(function(jqxhr, textStatus, error) {
+        fsqData = { error_text: textStatus + ": " + error };
+      });
+
+    console.log(fsqData);
+
+    var content = "<div id='content'><h1>" + marker.title + "</h1>" +
+      "<div id='bodyContent'>" + "<p><b>Nearest Foursquare Veunue: </b>" +
+      marker.title + "</p>" +
+      "</div></div>";
+
     infoWindow.marker = marker;
     infoWindow.addListener('closeclick', function() {
       infoWindow.marker = null;
     });
-    infoWindow.setContent("<div>" + marker.title + "</div>");
+    infoWindow.setContent(content);
     infoWindow.open(Neighborhood, marker);
   }
 }
